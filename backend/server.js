552a6dc551430e5e0d0a9eb784ca89801ff91879
Benjamin King <kingbenjamind@gmail.com>
2023-03-14 12:03:49 -0700

    Nearly successful run on heroku

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ if(process.env.NODE_ENV==='production') {
     app.use(express.static(path.join(__dirname +'/../build/')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join((__dirname + '/../build/public/static/js/index.html')));
+        res.sendFile(path.join((__dirname + '/../build/index.html')));
   });
 }
 
