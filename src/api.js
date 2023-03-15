8dcb7258026d71eaabe9b38721cf07bd64a0b8d0
Benjamin King <kingbenjamind@gmail.com>
2023-03-15 12:34:29 -0700

    First fully successful Heroku deployment

--- src/api.js
+++ src/api.js
@@ -1,11 +1,5 @@
 import axios from "axios";
 
-if (process.env.NODE_ENV ==="production") {
-    const BASE_URL = process.env.REACT_APP_API_URL
-} else {
-    const BASE_URL = `${process.env.REACT_APP_API_URL}${process.env.REACT_APP_SERVER_PORT}`
-}
-
 const BASE_URL = process.env.REACT_APP_API_URL
 const CLOUDNAME = process.env.REACT_APP_CLOUD_NAME
 const UPLOADPRESET = process.env.REACT_APP_UPLOAD_PRESET
